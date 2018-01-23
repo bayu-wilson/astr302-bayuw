d0d05be1808c265df23611756907db77383ad085
Bayu Wilson <bayuw@uw.edu>
2018-01-23 15:20:25 -0800

    2 jan23

--- jan23.py
+++ jan23.py
@@ -1,5 +1,10 @@
+#!/Users/bayuwilson/anaconda3/bin/python
 
 import functools
-return functools.reduce(lambda x,y:x*y)
+import sys
 
+def juric_func(*arg): 
+	return functools.reduce(lambda x,y:float(x)*float(y),arg)
+juric_func(sys.argv[1:])
+#print(sys.argv)
 
